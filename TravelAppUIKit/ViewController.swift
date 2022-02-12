488c17c100680afab586cf46674b434b903c2fc2
MostafaD92 <mostafa@curatio.me>
2022-02-12 10:48:02 -0800

    Initial commit

---
+++ TravelAppUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelAppUIKit
+//
+//  Created by Mostafa Davoodi on 2/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
